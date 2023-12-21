8e7dfd6f5df0dcfac062ae2406854562a71d781d
Alexbabu8 <112873625+Alexbabu8@users.noreply.github.com>
2023-12-21 14:11:00 +0530

    the database has been switched back to postgresql by using online services instead of local device storage.

--- database.py
+++ database.py
@@ -5,20 +5,22 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #SQLite below:
 
-SQLALCHEMY_DATABASE_URL='sqlite:///todosapp.db' #setting a directory to ceeate and store sqlite db file, running the code will automatically create a db file 
-engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base=declarative_base()
+# SQLALCHEMY_DATABASE_URL='sqlite:///todosapp.db' #setting a directory to ceeate and store sqlite db file, running the code will automatically create a db file 
+# engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+# Base=declarative_base()
 
 
 
 #postgreSQL below:
 
-# SQLALCHEMY_DATABASE_URL='postgresql://postgres:root@localhost/db_todoapp'
+SQLALCHEMY_DATABASE_URL='postgresql://kkujrvei:08uXLmqtGSxXmbc_6aQ1X9vTAOdRwyzr@rain.db.elephantsql.com/kkujrvei'
 
-# engine=create_engine(SQLALCHEMY_DATABASE_URL)
+#the above link was copied from elephantsql.com. the account used was alexbabu1998@gmail.com
 
-# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 
-# Base=declarative_base()
+Base=declarative_base()
