3afbbfcc4e9fea458a2ca5af653c0504a0072f65
Alexbabu8 <112873625+Alexbabu8@users.noreply.github.com>
2023-12-21 12:04:00 +0530

    Database switched from postgres to Sqlite

--- database.py
+++ database.py
@@ -5,20 +5,20 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #SQLite below:
 
-# SQLALCHEMY_DATABASE_URL='sqlite:///todosapp.db' #setting a directory to ceeate and store sqlite db file, running the code will automatically create a db file 
-# engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
-# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-# Base=declarative_base()
+SQLALCHEMY_DATABASE_URL='sqlite:///todosapp.db' #setting a directory to ceeate and store sqlite db file, running the code will automatically create a db file 
+engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
 
 
 
 #postgreSQL below:
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:root@localhost/db_todoapp'
+# SQLALCHEMY_DATABASE_URL='postgresql://postgres:root@localhost/db_todoapp'
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL,pool_size=10,max_overflow=20)
+# engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 
-Base=declarative_base()
+# Base=declarative_base()
